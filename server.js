ab1087ab4e7dcc6110801fb8bc74dc72a9a9b7c8
manishrajdoot <136050008+manishrajdoot@users.noreply.github.com>
2025-05-04 00:07:30 +0530

    Merge branch 'main' of https://github.com/manishrajdoot/Azureal

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -35,45 +34,4 @@ io.on('connection', (socket) => {
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
-=======
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server, {
-    cors: { 
-        origin: "*", // सभी डोमेन से कनेक्शन की अनुमति देता है
-        methods: ["GET", "POST"] 
-    }
-});
-
-const port = process.env.PORT || 3001;
-
-app.use(express.static('public'));
-
-io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    socket.on('offer', (offer) => {
-        socket.broadcast.emit('offer', offer);
-    });
-
-    socket.on('answer', (answer) => {
-        socket.broadcast.emit('answer', answer);
-    });
-
-    socket.on('ice-candidate', (candidate) => {
-        socket.broadcast.emit('ice-candidate', candidate);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
-});
-
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
->>>>>>> Stashed changes
 });
